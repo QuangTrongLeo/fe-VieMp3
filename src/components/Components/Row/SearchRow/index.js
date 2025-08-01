c58bbd527d9f594332b676b62ce461ea6c5abe14
QuangTrongLeo <trongphamtg05@gmail.com>
2025-08-02 00:11:57 +0700

    fix: update SearchRow component, update Tippy popup and process render Tippy popup dependent on keyword from input of form

--- src/components/Components/Row/SearchRow/index.js
+++ src/components/Components/Row/SearchRow/index.js
@@ -4,11 +4,18 @@ import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
 
-function SearchRow({ cover, content, desc }) {
+function SearchRow({ type, cover, content, desc }) {
+  const path = type === 'artist' ? `/artist/${encodeURIComponent(content)}` : `/song/${encodeURIComponent(content)}`;
+
   return (
-    <a href={`/song/${content}`} className={cx('search-row-link')}>
-      <div className={cx('search-row', 'd-flex', 'align-items-center', 'px-3', 'py-2')}>
-        <img src={cover} alt={content} className={cx('search-row-cover', 'me-3')} />
+    <a href={path} className={cx('search-row-link')}>
+      <div className={cx('search-row', 'd-flex', 'align-items-center', 'px-3', 'py-1')}>
+        <img
+          src={cover}
+          alt={content}
+          className={cx('search-row-cover', 'me-3', { 'artist-avatar': type === 'artist' })}
+        />
+
         <div>
           <div className={cx('search-row-content')}>{content}</div>
           <div className={cx('search-row-desc', 'small')}>{desc}</div>
