c10d3dc79f1f9c36d50dff2e0eeeb4ef1c07e9fe
QuangTrongLeo <trongphamtg05@gmail.com>
2025-08-03 10:02:30 +0700

    feat: create PlayButton component

--- src/components/Components/Button/PlayButton/index.js
+++ src/components/Components/Button/PlayButton/index.js
@@ -1,5 +1,15 @@
-function PlayButton() {
-  return <h1>Play btn</h1>;
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './PlayButton.module.scss';
+
+const cx = classNames.bind(styles);
+
+function PlayButton({ onClick }) {
+  return (
+    <button className={cx('play-btn')} onClick={onClick}>
+      <i className="fas fa-play"></i>
+    </button>
+  );
 }
 
 export default PlayButton;
