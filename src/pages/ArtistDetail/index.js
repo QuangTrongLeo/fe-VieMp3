9c89d1db622c4a98cbbe0852917a3434f15886a1
QuangTrongLeo <trongphamtg05@gmail.com>
2025-08-05 19:54:54 +0700

    fix: update ArtistDetail page

--- src/pages/ArtistDetail/index.js
+++ src/pages/ArtistDetail/index.js
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import styles from './ArtistDetail.module.scss';
 import SongItem from '~/components/Components/SongItem';
 import { audios } from '~/assets';
+import LimitedList from '~/components/Components/LimitedList';
 
 const cx = classNames.bind(styles);
 
@@ -50,7 +51,7 @@ function ArtistDetail() {
       <div className="row">
         {/* Mới phát hành */}
         <div className="col-md-4 mb-4">
-          <h5 className={cx('section-title')}>Mới Phát Hành</h5>
+          <h5 className={cx('section-title', 'mb-4')}>Mới Phát Hành</h5>
           <div className={cx('release-card')}>
             <img
               src="https://photo-resize-zmp3.zmdcdn.me/w165_r1x1_webp/cover/8/7/3/7/8737cb85fcd8108109bdb819df8f0d5d.jpg"
@@ -70,13 +71,23 @@ function ArtistDetail() {
           <div className="d-flex justify-content-between align-items-center mb-2">
             <h5 className={cx('section-title')}>Bài Hát Nổi Bật</h5>
           </div>
-          <div className="row">
+          {/* <div className="row">
             {hotSongs.map((song, idx) => (
               <div className="col-md-6 mb-3" key={idx}>
                 <SongItem song={song} />
               </div>
             ))}
-          </div>
+          </div> */}
+          <LimitedList
+            items={hotSongs}
+            limit={10}
+            wrapInRow={true}
+            renderItem={(song, idx) => (
+              <div className="col-md-6 mb-3" key={idx}>
+                <SongItem song={song} />
+              </div>
+            )}
+          />
         </div>
       </div>
     </div>
