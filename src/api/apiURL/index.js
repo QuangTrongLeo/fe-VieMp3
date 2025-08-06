20b77bf44574f587c52a2f24a36081ba8a617cfd
QuangTrongLeo <trongphamtg05@gmail.com>
2025-08-06 07:59:21 +0700

    fix: update Genres page; feat: create apiGenres to Genres page import

---
+++ src/api/apiURL/index.js
@@ -0,0 +1,1 @@
+export { default as apiGenres } from './apiGenres';
--- src/pages/Genres/index.js
+++ src/pages/Genres/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { RectangleCard } from '~/components/Components/Card';
 import icons from '~/assets/icons';
 import styles from './Genres.module.scss';
 import classNames from 'classnames/bind';
 import LimitedList from '~/components/Components/LimitedList';
+import { RectangleCard } from '~/components/Components/Card';
+import { apiGenres } from '~/api/apiURL';
 
 const cx = classNames.bind(styles);
-
-const apiGenres = [
-  { id: 1, genreName: 'RAP', genreDesc: 'Rap / Hip Hop Việt' },
-  { id: 2, genreName: 'POP', genreDesc: 'Nhạc Pop Việt' },
-  { id: 3, genreName: 'BALLAD', genreDesc: 'Nhạc Ballad' },
-  { id: 4, genreName: 'RNB', genreDesc: 'R&B Việt' },
-  { id: 5, genreName: 'EDM', genreDesc: 'Nhạc EDM' },
-  { id: 6, genreName: 'INDIE', genreDesc: 'Indie Việt' },
-  { id: 7, genreName: 'LOFI', genreDesc: 'Nhạc Lofi Chill' },
-  { id: 8, genreName: 'DANCE', genreDesc: 'Nhạc Dance Việt' },
-  { id: 9, genreName: 'REMIX', genreDesc: 'Nhạc Remix Việt' },
-  { id: 10, genreName: 'ACOUSTIC', genreDesc: 'Nhạc Acoustic Việt' },
-  { id: 11, genreName: 'TRINH_CONG_SON', genreDesc: 'Nhạc Trịnh Công Sơn' },
-];
 
 function Genres() {
   return (
